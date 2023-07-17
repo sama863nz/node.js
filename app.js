7f1bf9677a2cd3aa4e8a6cd571899ab24bca3660
Sam <sam.a863.nz@gmail.com>
2023-07-17 13:59:24 +1200

    new files added

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var port = process.env.PORT || 3000,
+const port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs'),
     html = fs.readFileSync('index.html');
 
-var log = function(entry) {
+const log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
 
-var server = http.createServer(function (req, res) {
+const server = http.createServer(function (req, res) {
     if (req.method === 'POST') {
-        var body = '';
+        let body = '';
 
         req.on('data', function(chunk) {
             body += chunk;
